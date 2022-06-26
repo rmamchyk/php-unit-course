826a8e9fa660d37cb22b1f609db497f625ef105b
rmamchyk <roman.mamchyk@gmail.com>
2022-06-26 12:56:00 +0300

    add first test

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -4,4 +4,8 @@ use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase {
 
+    public function testAddingTwoPlusTwoResultsInFour() {
+        $this->assertEquals(4, 2 + 2);
+    }
+
 }
