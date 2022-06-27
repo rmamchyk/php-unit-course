7e6d393ad58f3e95a419edbbdaaa97e4bd13302d
rmamchyk <roman.mamchyk@gmail.com>
2022-06-27 15:53:18 +0300

    add queue test

---
+++ src/Queue.php
@@ -0,0 +1,17 @@
+<?php
+
+class Queue {
+    protected $items = [];
+
+    public function push($item) {
+        $this->items[] = $item;
+    }
+
+    public function pop() {
+        return array_pop($this->items);
+    }
+
+    public function getCount() {
+        return count($this->items);
+    }
+}
